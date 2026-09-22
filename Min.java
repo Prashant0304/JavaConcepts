1e84c1b8e2e74f7de5795f744f93df6eff632111
Prashant0304 <prashantpattanashetti5@gmail.com>
2026-09-22 18:24:44 +0530

    Array concepts with length , comparator operator and initialisation of variable

---
+++ Min.java
@@ -0,0 +1,12 @@
+class Min{
+	public static void main(String args[]){
+		int arr[]={32,54,212,11,332,43};
+		int min=arr[0];
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]<min){
+				min=arr[i];
+			}
+		}
+		System.out.println("Minimum value in given array is "+min);
+	}
+}
